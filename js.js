3232345b7100ea27956bb377cc0b4ed6c1fbe699
pooyan <thepooyan@gmail.com>
2025-10-09 14:16:37 +0330

    nothing!

--- js.js
+++ js.js
@@ -180,10 +180,16 @@ function handleVerificationSubmit(e) {
         openCartModal();
     } else {
         // Proceed with lottery
+        const svgStr = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path fill="#10b222" d="M256 512a256 256 0 1 1 0-512 256 256 0 1 1 0 512zM374 145.7c-10.7-7.8-25.7-5.4-33.5 5.3L221.1 315.2 169 263.1c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l72 72c5 5 11.8 7.5 18.8 7s13.4-4.1 17.5-9.8L379.3 179.2c7.8-10.7 5.4-25.7-5.3-33.5z"/></svg>`
+        const parser = new DOMParser()
+        const svgElement = parser.parseFromString(svgStr, 'image/svg+xml').documentElement
         document.getElementById('verification-content').classList.add('hidden');
         document.getElementById('verification-success').classList.remove('hidden');
         document.querySelector('#verification-modal .note').classList.add('hidden')
-        document.getElementById('verification-title').textContent = 'موفقیت‌آمیز بود!';
+        const successTitle = document.getElementById('verification-title');
+        successTitle.textContent = 'موفقیت‌آمیز بود!';
+        successTitle.insertBefore(svgElement, successTitle.firstChild)
+        successTitle.classList.add("success")
         
         const { name, mobile, school } = lotteryUserData;
         alert(JSON.stringify(lotteryUserData, null, 2))
